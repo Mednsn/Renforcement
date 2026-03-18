02a2d31f00cfbf47140290264d5be3328bd1d26c
mohamed <medolgholb@gmail.com>
2026-03-18 01:31:08 +0100

    jai fais challenge 1 de semaine 1

---
+++ Semaine1/day1/souvetage/challenge-1.js
@@ -0,0 +1,5 @@
+let animaux =['dog','cat','giraf','cow','tiger'];
+
+console.log(animaux[0]);
+console.log(animaux[4]);
+console.log(animaux.length);
---
+++ Semaine1/day1/souvetage/challenge-2.js
@@ -0,0 +1,9 @@
+let colors = ["rouge", "bleu", "vert"];
+colors.push('jaune');
+console.log(colors);
+colors.pop('jaune');
+console.log(colors);
+colors.shift('jaune');
+console.log(colors);
+colors.unshift('noir');
+console.log(colors);
